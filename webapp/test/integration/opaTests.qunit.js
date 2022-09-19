64918182a042362e8d26a12dc9340f32411ab42a
VeroMontes <veronica.montes@babelgroup.com>
2022-09-19 14:54:32 +0000

    First Commit on development

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["Banel/invoice/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Banel/invoice/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
